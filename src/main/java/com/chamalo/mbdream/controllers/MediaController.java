59674c76e1597abe6c633c82cf4bd463a9f4b06a
Valentin <vltn.justin@gmail.com>
2021-05-21 19:53:18 +0200

    Change url for getting one media, remove old method

--- src/main/java/com/chamalo/mbdream/controllers/MediaController.java
+++ src/main/java/com/chamalo/mbdream/controllers/MediaController.java
@@ -6,10 +6,6 @@ import com.chamalo.mbdream.responses.MediaResponse;
 import com.chamalo.mbdream.responses.ResponseType;
 import com.chamalo.mbdream.services.MediaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,12 +13,10 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -33,7 +27,7 @@ import java.util.Map;
  * @author Chamalo
  */
 @CrossOrigin(origins = {"http://localhost:4200",
-        "https://motorbike-dream.web.app"}) // Authorize angular
+        "https://motorbike-dream.web.app"})
 @RestController
 @RequestMapping("/media")
 public class MediaController {
@@ -67,12 +61,12 @@ public class MediaController {
      * Method to get all media from moto
      *
      * @param slugMoto Slug of moto
-     * @param isVideo  Boolean to know if you want videos or images
      *
      * @return ResponseEntity
      */
-    @GetMapping("/get/{slugMoto}/{isVideo}")
-    public ResponseEntity<List<Map<String, Object>>> getAllMedia(@PathVariable final String slugMoto, @PathVariable final Boolean isVideo) {
+    @GetMapping("/get/{slugMoto}")
+    public ResponseEntity<List<Map<String, Object>>> getAllMedia(@PathVariable final String slugMoto,
+                                                                 @RequestParam(value = "isVideo", defaultValue = "false") final Boolean isVideo) {
         Iterable<MediaModel> allMedia = this.mediaService.findAllMedia(slugMoto, isVideo);
 
         List<Map<String, Object>> mapList = new ArrayList<>();
@@ -95,36 +89,5 @@ public class MediaController {
     public ResponseEntity<String> deleteMedia(@PathVariable final String idMedia) {
         this.mediaService.deleteMedia(idMedia);
         return ResponseEntity.ok("Media supprimer");
-    }
-
-    /**
-     * Method to get one media
-     *
-     * @param isVideo String to find video or images
-     * @param type    Type of img, if it's for moto or marque
-     * @param slug    Slug of moto or marque
-     * @param nom     Name of media + format
-     *
-     * @return ResponseEntity
-     *
-     * @throws IOException Exception for file
-     */
-    @GetMapping(value = "/{isVideo}/{type}/{slug}/{nom}")
-    @ResponseBody
-    public ResponseEntity<Object> getOneImg(@PathVariable final String isVideo,
-                                            @PathVariable final String type,
-                                            @PathVariable final String slug,
-                                            @PathVariable final String nom) throws IOException {
-        Path path = Paths.get("/home/pi/mbdream-spring/resources/static/" + isVideo + "/" + type + "/" + slug + "/");
-        Resource resource = new UrlResource(path.resolve(nom).toUri());
-
-        if (resource.exists() || resource.isReadable()) {
-            return ResponseEntity.ok()
-                    .header(HttpHeaders.CONTENT_DISPOSITION,
-                            "inline")
-                    .contentType(MediaType.parseMediaType("image/png"))
-                    .body(resource);
-        }
-        return ResponseEntity.noContent().build();
     }
 }
