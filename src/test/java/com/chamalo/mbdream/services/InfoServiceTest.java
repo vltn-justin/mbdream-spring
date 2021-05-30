baedda01e1df791d20bfe03eccb75139cfcea1cd
Valentin J <vltn.justin@gmail.com>
2021-05-30 12:43:00 +0200

    Test KO for add

--- src/test/java/com/chamalo/mbdream/services/InfoServiceTest.java
+++ src/test/java/com/chamalo/mbdream/services/InfoServiceTest.java
@@ -1,10 +1,13 @@
 package com.chamalo.mbdream.services;
 
+import com.chamalo.mbdream.dto.InfoDTO;
 import com.chamalo.mbdream.exceptions.MBDreamException;
 import com.chamalo.mbdream.models.InfoModel;
+import com.chamalo.mbdream.models.MotoModel;
 import com.chamalo.mbdream.repositories.InfoRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,14 +21,30 @@ class InfoServiceTest {
 	@MockBean
 	private InfoRepository repository;
 
+	@MockBean
+	private MotoService motoService;
+
 	@Autowired
 	private InfoService service;
+
+	/**
+	 * Test KO for {@link InfoService#addInfo(InfoDTO)}
+	 */
+	@Test
+	void testAddKO() {
+		final InfoDTO infoDTO = new InfoDTO();
+		infoDTO.setSlugMoto("test");
+
+		Mockito.when(this.motoService.findMotoBySlug("test")).thenThrow(new MBDreamException("Moto introuvable"));
+
+		Assertions.assertThrows(MBDreamException.class, () -> this.service.addInfo(infoDTO));
+	}
 
 	/**
 	 * Test for OK {@link InfoService#findInfoMoto(String)}
 	 */
 	@Test
-	void testFindInfoOk() {
+	void testFindInfoOK() {
 		final InfoModel infoModel = new InfoModel(1L, 5000, "architecture", 650, 110, 12, 180, 17D, 12.5D, null);
 
 		Mockito.when(this.repository.findInfoMoto("test")).thenReturn(Optional.of(infoModel));
