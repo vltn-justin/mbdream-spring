beafbadb9b0a2ce547039add2e89fe9eddcfb64b
Valentin <vltn.justin@gmail.com>
2021-05-31 08:23:21 +0200

    Test count

--- src/test/java/com/chamalo/mbdream/controllers/MotoControllerTest.java
+++ src/test/java/com/chamalo/mbdream/controllers/MotoControllerTest.java
@@ -1,9 +1,14 @@
 package com.chamalo.mbdream.controllers;
 
+import com.chamalo.mbdream.exceptions.MBDreamException;
 import com.chamalo.mbdream.services.MotoService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class MotoControllerTest {
@@ -13,4 +18,17 @@ class MotoControllerTest {
 
 	@Autowired
 	private MotoController controller;
+
+	/**
+	 * Test for {@link MotoController#countAllMoto()}
+	 */
+	@Test
+	void testCount() {
+		Mockito.when(this.service.countAllMoto()).thenReturn(5L);
+
+		final ResponseEntity<Long> count = this.controller.countAllMoto();
+
+		Assertions.assertNotNull(count);
+		Assertions.assertEquals(5L, count.getBody());
+	}
 }
