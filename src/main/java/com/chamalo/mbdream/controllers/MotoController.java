cd8568f2fe6e33060bdcc5707020a6f1141f31b9
Valentin <vltn.justin@gmail.com>
2021-05-31 19:25:47 +0200

    Import for sonnar

--- src/main/java/com/chamalo/mbdream/controllers/MotoController.java
+++ src/main/java/com/chamalo/mbdream/controllers/MotoController.java
@@ -10,9 +10,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Rest Controller for Moto
